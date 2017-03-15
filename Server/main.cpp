f9e900fd8b13ff7cbf133c463d134e5371247f16
Brandon Torgerson <btorgerson+github@gmail.com>
2017-03-14 22:20:14 -0700

    Forgot to fix a known race condition

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,14 +1,18 @@
 #include "Network.h"
 
-#include <sys/socket.h>
-
 #include <boost/asio.hpp>
+#include <chrono>
+#include <thread>
 
 int main(int argc, char** argv)
 {
     boost::asio::io_service io_service;
     Network network(&io_service);
     network.Start();
+
+    //Not so good way of preventing a race condition
+    //A better way would be the use of signals
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 
     io_service.run();
 }
